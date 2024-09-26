0dea54dc262dc24ddc9e7629afd9d3907b1d7095
Yaroslav Mudrevskyi <yarikyaroslav13200@gmail.com>
2024-09-26 15:31:49 +0300

    create some components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import WelcomeSection from "./components/WelcomeSection/WelcomeSectiom";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <WelcomeSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/WelcomeSection/WelcomeSectiom.jsx
@@ -0,0 +1,22 @@
+const WelcomeSection = () => {
+  return (
+    <section>
+      <button>Click</button>
+      <button>Login</button>
+      <button>Register</button>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur at
+        quia obcaecati dignissimos velit, doloribus vero quaerat doloremque!
+        Perspiciatis, sed!
+      </p>
+      <ul>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+      </ul>
+    </section>
+  );
+};
+
+export default WelcomeSection;

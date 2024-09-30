10e5745179c5e78a68d2b8c5c7aeec933c7f7db0
Yaroslav Mudrevskyi <yarikyaroslav13200@gmail.com>
2024-09-30 13:57:11 +0300

    add first useState for Counter

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,29 +1,35 @@
+import { useState } from "react";
 import s from "./Counter.module.css";
 
 const Counter = () => {
+  const [counter, setCounter] = useState(1);
+
   const handleMinusClick = () => {
-    //   console.log({ x: e.pageX, y: e.pageY });
-    //   e.target.innerHTML = "Click";
-    console.log("Welcome");
+    if (counter < 1) return;
+    setCounter(counter - 1);
   };
 
-  const handleResetClick = (name) => {
-    console.log(`Hello, ${name}`);
+  const handleResetClick = () => {
+    setCounter(1);
+  };
+
+  const handlePlusClick = () => {
+    setCounter(counter + 1);
   };
 
   return (
     <div className={s.flexContainer}>
       <div className={s.wrapper}>
-        <h1>{1}</h1>
+        <h1>{counter}</h1>
         <input />
         <div className={s.flex}>
           <button onClick={handleMinusClick} className={s.btn}>
             minus
           </button>
-          <button onClick={() => handleResetClick("Vasya")} className={s.btn}>
+          <button onClick={handleResetClick} className={s.btn}>
             reset
           </button>
-          <button onClick={() => console.log("Hello, user")} className={s.btn}>
+          <button onClick={handlePlusClick} className={s.btn}>
             plus
           </button>
         </div>
